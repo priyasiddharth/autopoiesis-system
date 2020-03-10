78cdde6b0e75a22666e077c0a747c9a252e936ed
Siddharth Priya <priya.siddharth@gmail.com>
2020-03-09 23:40:47 -0400

    Implemented 1.3 and 1.32

--- test_world_model.py
+++ test_world_model.py
@@ -5,9 +5,9 @@ from world_model import *
 
 class ChooseFirstStrategy(ChooseStrategy):
 
-    def chooseOne(self, *args):
+    def chooseOne(self, args: [Element]) -> Element:
         assert len(args) > 0
-        return args[1]
+        return args[0]
 
 
 class TestElement(TestCase):
@@ -33,3 +33,25 @@ class TestElement(TestCase):
         # RU corner
         e = Element(Point(4, 0), 5)
         self.assertCountEqual([(3, 0), (4, 1)], e.getNeighbours(), e)
+
+
+class TestHoleProcess(TestCase):
+    def test_do_step(self):
+        h = Hole(Point(0, 0), 2)
+        k = Catalyst(Point(0, 1), 2)
+        s = Substrate(Point(1, 0), 2)
+        l = Link(Point(1, 1), 2)
+        grid = {Point(0, 0): h, Point(0, 1): k, Point(1, 0): s, Point(1, 1): l}
+        hole_list = [h]
+        substrate_list = [s]
+        link_list = [l]
+        catalyst_list = [k]
+        choose_strategy = ChooseFirstStrategy()
+        logger = logging.getLogger('test')
+        hp = HoleProcess(grid, hole_list, substrate_list, catalyst_list, link_list, choose_strategy, logger)
+        hp.doStep()
+        self.assertDictEqual({Point(0, 0): Catalyst(Point(0, 0), 2),
+                              Point(0, 1): Hole(Point(0, 1), 2),
+                              Point(1, 0): s,
+                              Point(1, 1): l},
+                             grid)
