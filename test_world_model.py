11f74dac113e2692fdbf67541b58608d4379488d
Siddharth Priya <priya.siddharth@gmail.com>
2020-03-10 19:11:30 -0400

    Implemented 1 and 2 except bonding. Fixed getNeighbours - was not accounting for diagnals.

--- test_world_model.py
+++ test_world_model.py
@@ -16,23 +16,23 @@ class TestElement(TestCase):
 
     def test_get_neighbours(self):
         e = Element(Point(5, 5), 10)
-        self.assertCountEqual([(4, 5), (5, 4), (6, 5), (5, 6)], e.getNeighbours(), e)
+        self.assertCountEqual([(4, 4), (6, 4), (6, 6), (4, 6), (4, 5), (5, 4), (6, 5), (5, 6)], e.getNeighbours())
 
         # LU corner
         e = Element(Point(0, 0), 5)
-        self.assertCountEqual([(0, 1), (1, 0)], e.getNeighbours(), e)
+        self.assertCountEqual([(0, 1), (1, 0), (1, 1)], e.getNeighbours())
 
         # LL corner
         e = Element(Point(0, 4), 5)
-        self.assertCountEqual([(0, 3), (1, 4)], e.getNeighbours(), e)
+        self.assertCountEqual([(0, 3), (1, 4), (1, 3)], e.getNeighbours())
 
         # RL corner
         e = Element(Point(4, 4), 5)
-        self.assertCountEqual([(4, 3), (3, 4)], e.getNeighbours(), e)
+        self.assertCountEqual([(4, 3), (3, 4), (3, 3)], e.getNeighbours())
 
         # RU corner
         e = Element(Point(4, 0), 5)
-        self.assertCountEqual([(3, 0), (4, 1)], e.getNeighbours(), e)
+        self.assertCountEqual([(3, 0), (4, 1), (3, 1)], e.getNeighbours())
 
 
 class TestHoleProcess(TestCase):
--- world_model.py
+++ world_model.py
@@ -41,55 +41,69 @@ class Element(object):
             return True
         return False
 
+    # TODO: this fn may not be needed
+    def hasNeighbourOfType(self, c, grid: Dict[Point, 'Element']) -> bool:
+        nbours = self.getNeighbours()
+        for n in nbours:
+            n_o = grid[n]
+            if isinstance(n_o, c):
+                return True
+        return False
+
+    def getNeighboursofType(self, c, grid: Dict[Point, 'Element']) -> ['Element']:
+        assert self.hasNeighbourOfType()
+        nbours = self.getNeighbours()
+        l = []
+        for n in nbours:
+            n_o = grid[n]
+            if isinstance(n_o, c):
+                l.append(n_o)
+        return l
+
     def getNeighbours(self) -> [Point]:
+        return self._getNeighbours(1)
+
+    def _getNeighbours(self, d: int) -> [Point]:
         """
 
-        :return: A list of Points
-        """
+          :return: A tuple of Points
+          """
         n = []
         x = self.point.x
         y = self.point.y
 
         # north
-        if y - 1 >= 0:
-            n.append(Point(x, y - 1))
+        if y - d >= 0:
+            n.append(Point(x, y - d))
         # south
-        if y + 1 < self.grid_size:
-            n.append(Point(x, y + 1))
+        if y + d < self.grid_size:
+            n.append(Point(x, y + d))
 
         # east
-        if x + 1 < self.grid_size:
-            n.append(Point(x + 1, y))
+        if x + d < self.grid_size:
+            n.append(Point(x + d, y))
 
         # west
-        if x - 1 >= 0:
-            n.append(Point(x - 1, y))
+        if x - d >= 0:
+            n.append(Point(x - d, y))
+
+        if d < 2:
+            # NW
+            if y - d >= 0 and x - d >= 0:
+                n.append(Point(x - d, y - d))
+            # NE
+            if y - d >= 0 and x + d < self.grid_size:
+                n.append(Point(x + d, y - d))
+            # SE
+            if y + d < self.grid_size and x + d < self.grid_size:
+                n.append(Point(x + d, y + d))
+            # SW
+            if y + d < self.grid_size and x - d >= 0:
+                n.append(Point(x - d, y + d))
         return n
 
     def getExtendedNeighbours(self) -> [Point]:
-        """
-
-        :return: A tuple of Points
-        """
-        n = []
-        x = self.point.x
-        y = self.point.y
-
-        # north
-        if y - 1 >= 0:
-            n.append(Point(x, y - 1))
-        # south
-        if y + 1 < self.grid_size:
-            n.append(Point(x, y + 1))
-
-        # east
-        if x + 1 < self.grid_size:
-            n.append(Point(x + 1, y))
-
-        # west
-        if x - 1 >= 0:
-            n.append(Point(x - 1, y))
-        return n
+        return self._getNeighbours(2)
 
     def chooseNeighbour(self, chooser: ChooseStrategy) -> 'Element':
         return chooser.chooseOne(self.getNeighbours())
@@ -130,6 +144,9 @@ class Substrate(Element):
             return True
         else:
             return False
+
+    def getNeighbourofType(self, type, grid):
+        return super().getNeighboursofType(type, grid)
 
 
 class Catalyst(Element):
@@ -181,7 +198,7 @@ class Process(object):
         self.logger: logging.Logger = logger
 
     def swap(self, this: Element, other: Element):
-        assert this.isNeighbour(other)
+        # assert this.isNeighbour(other)
         temp = self.grid[this.point]
         self.grid[this.point] = other
         self.grid[other.point] = temp
@@ -207,12 +224,59 @@ class HoleProcess(Process):
             elif isinstance(n, Link):
                 if n.isFree():
                     self.swap(hole, n)
+                # 1.32 'L is bonded, swap with extended neighbour S'
+                else:
+                    s_list = [s for s in n.getNeighboursofType(Substrate, self.grid)]
+                    en_list = hole.getExtendedNeighbours()
+                    # find the right extended neighbour
+                    common_s = [e for e in en_list and e in s_list]
+                    assert len(common_s) == 1
+                    self.swap(hole, common_s[0])
             # 1.31
             elif isinstance(n, Hole):
                 # do nothing
                 continue
+            # TODO: 1.4 Bond any moved L
+
+
+class LinkProcess(Process):
+
+    def __init__(self, grid: Dict[Point, Element], hole_list: List[Hole], substrate_list: List[Substrate],
+                 catalyst_list: List[Catalyst], link_list: [List], choose_strategy: ChooseStrategy,
+                 logger: logging.Logger):
+        super().__init__(grid, hole_list, substrate_list, catalyst_list, link_list, choose_strategy, logger)
+
+    def doStep(self):
+        free_l_list = [l for l in self.l_list if l.isFree()]
+        for link in free_l_list:
+            n = self.grid[link.chooseNeighbour(self.chooser)]
+            # 2.32 'S will be displaced'
+            if isinstance(n, Substrate):
+                # 2.321
+                if n.hasNeighbourOfType(Hole, self.grid):
+                    nh = self.chooser.chooseOne(n.getNeighbourofType(Hole, self.grid))
+                    self.swap(n, nh)
+                # 2.322
+                elif n.hasNeighbourOfType(Link, self.grid):
+                    links = n.getNeighboursofType(Link, self.grid)
+                    # remove 'this'
+                    links.remove(link)
+                    l = self.chooser.chooseOne([l for l in links if not l.isFree()])
+                    h = self.chooser.chooseOne([h for h in l.getNeighboursofType(Hole, self.grid)])
+                    self.swap(n, h)
+                # 2.323
+                else:
+                    self.swap(link, n)
+            # 2.33
+            elif isinstance(n, Hole):
+                self.swap(link, n)
+            # 2.31
+            elif isinstance(n, (Link, Catalyst)):
+                # do nothing
+                continue
             # 1.32
             # TODO: Add bonded L routine
+            # TODO: Bond any moved L
 
 
 class WorldModel(object):
